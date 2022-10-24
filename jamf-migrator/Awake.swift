de151a9d85d69f7d66dfba33fca4ad36d84a4bbe
Leslie Helou <leslie@ccs-indy.com>
2022-10-24 14:14:31 -0400

    cleanup

--- jamf-migrator/Awake.swift
+++ jamf-migrator/Awake.swift
@@ -10,31 +10,14 @@ import Foundation
 import IOKit.pwr_mgt
 
 var noSleepAssertionID: IOPMAssertionID = 0
-var noSleepReturn: IOReturn? // Could probably be replaced by a boolean value, for example 'isBlockingSleep', just make sure 'IOPMAssertionRelease' doesn't get called, if 'IOPMAssertionCreateWithName' failed.
-var noSleepReturn2: IOReturn?
-
-//public func disableScreenSleep(reason: String) -> Bool? {
-//    guard noSleepReturn == nil else { return nil }
-//    noSleepReturn = IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString,
-//                                            IOPMAssertionLevel(kIOPMAssertionLevelOn),
-//                                            reason as CFString,
-//                                            &noSleepAssertionID)
-//}
+var noSleepReturn: IOReturn?
 
 public func disableSleep(reason: String) -> Bool? {
-    guard noSleepReturn2 == nil else { return nil }
-    noSleepReturn2 = IOPMAssertionCreateWithName(kIOPMAssertPreventUserIdleSystemSleep as CFString,IOPMAssertionLevel(kIOPMAssertionLevelOn), reason as CFString, &noSleepAssertionID)
+    guard noSleepReturn == nil else { return nil }
+    noSleepReturn = IOPMAssertionCreateWithName(kIOPMAssertPreventUserIdleSystemSleep as CFString,IOPMAssertionLevel(kIOPMAssertionLevelOn), reason as CFString, &noSleepAssertionID)
     return noSleepReturn == kIOReturnSuccess
 }
 
-//public func enableScreenSleep() -> Bool {
-//    if noSleepReturn != nil {
-//        _ = IOPMAssertionRelease(noSleepAssertionID) == kIOReturnSuccess
-//        noSleepReturn = nil
-//        return true
-//    }
-//    return false
-//}
 public func enableSleep() -> Bool {
     if noSleepReturn != nil {
         _ = IOPMAssertionRelease(noSleepAssertionID) == kIOReturnSuccess
